a4c8035fd1a3b39521d9f9d4d79774e293dbe934
lisz1012 <lisz1012@163.com>
2020-04-03 22:40:53 -0700

    Producer and consumer

--- src/main/java/com/lisz/producerconsumer/KafkaConsumerTest2.java
+++ src/main/java/com/lisz/producerconsumer/KafkaConsumerTest2.java
@@ -4,22 +4,35 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.regex.Pattern;
-// 同一个组之内的消息是有序的，有consumer加入或者退出，可能引起各个consumer对于topic下各个partition的rebalance。有可能一个consumer负责两个partition
-public class KafkaConsumerTest {
+
+/*
+不特别指定partition的时候，Producer发送消息的时候会让不同的Partitions负载均衡，均分各个消息；consumer指定了哪一个组的时候，同组的consumers
+对各个partitions进行分工，倾向于每人一个partition。这是Kafka的Consumer分组策略。当不指定consumer的组的时候（如下），consumers对于Partitions的
+自动均衡就没有了，这时候就得手动assign partitions，这时候还能同时指定从哪个offset开始读，多开几个相同的consumer实例也不会互相干扰
+ */
+
+public class KafkaConsumerTest2 {
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "Kafka_1:9092,Kafka_2:9092,Kafka_3:9092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "g1");
+        //props.put(ConsumerConfig.GROUP_ID_CONFIG, "g1");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-        consumer.subscribe(Pattern.compile("^topic.*"));
+        List<TopicPartition> partitions = Arrays.asList(new TopicPartition("topic01", 0));
+        consumer.assign(partitions);
+        //consumer.seekToBeginning(partitions);//从头开始消费
+        //consumer.seek(partitions.get(0), 1);//从指定的offset=1的位置开始消费
+        consumer.seek(new TopicPartition("topic01", 0), 2);//从指定的topic01的partition 0 的 offset=2的位置开始消费，这里的第二个参数要是上面assign的时候指定的：new TopicPartition("topic01", 0)
         while (true) {
             ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
             if (!consumerRecords.isEmpty()) { //从队列中取到了数据
@@ -30,19 +43,5 @@ public class KafkaConsumerTest {
                 });
             }
         }
-
-        //consumer.close();
     }
 }
-/* 同一个分区中消息是有序的，不同的分区综合起来看消息是无序的
-Topic: topic01 Partition: 0 Offset: 12 Key: lisz1 Value: 李书征1 Timestamp: 1585974667802
-Topic: topic01 Partition: 0 Offset: 13 Key: lisz2 Value: 李书征2 Timestamp: 1585974667802
-Topic: topic01 Partition: 0 Offset: 14 Key: lisz5 Value: 李书征5 Timestamp: 1585974667802
-Topic: topic01 Partition: 0 Offset: 15 Key: lisz8 Value: 李书征8 Timestamp: 1585974667803
-Topic: topic01 Partition: 2 Offset: 8 Key: lisz3 Value: 李书征3 Timestamp: 1585974667802
-Topic: topic01 Partition: 2 Offset: 9 Key: lisz6 Value: 李书征6 Timestamp: 1585974667802
-Topic: topic01 Partition: 2 Offset: 10 Key: lisz9 Value: 李书征9 Timestamp: 1585974667803
-Topic: topic01 Partition: 1 Offset: 9 Key: lisz0 Value: 李书征0 Timestamp: 1585974667795
-Topic: topic01 Partition: 1 Offset: 10 Key: lisz4 Value: 李书征4 Timestamp: 1585974667802
-Topic: topic01 Partition: 1 Offset: 11 Key: lisz7 Value: 李书征7 Timestamp: 1585974667803
- */
